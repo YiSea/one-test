a7b22deb97832c230fb4b83a9c57b06034b3034a
YiSea <smile_wj1025@163.com>
2017-03-02 10:50:36 +0800

    ...

--- src/main/java/com/one/test/Life.java
+++ src/main/java/com/one/test/Life.java
@@ -1,9 +1,15 @@
 package com.one.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Life {
-	public static void main(String args[]){
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("go");
+		br.readLine();
 		lineLife();
 	}
 	
